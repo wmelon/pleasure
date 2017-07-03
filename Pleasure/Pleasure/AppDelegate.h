6009ce7ee4eacb7be30e7f5991586a2eb3cd1793
陈仕家 <511863244@qq.com>
2017-07-03 10:47:34 +0800

    Pleasure项目初建

---
+++ Pleasure/Pleasure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pleasure
+//
+//  Created by Sper on 2017/7/3.
+//  Copyright © 2017年 WM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
